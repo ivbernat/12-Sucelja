d8e7be9957a9ad58f3eba0ebcb79da22c27ae88f
Ivan Bernatović <ivbernat@vsite.hr>
2016-12-22 20:22:55 +0100

    definicija implementacije

--- DefinicijaImplementacijaSucelja/DefinicijaImplementacijaSučelja.cs
+++ DefinicijaImplementacijaSucelja/DefinicijaImplementacijaSučelja.cs
@@ -20,8 +20,25 @@ namespace Vsite.CSharp
     // TODO: Napišite kod klase Implementacija tako da implementira sučelje IMojeSučelje. 
     // TODO: Implementirajte metodu GenerirajMojDogađaj tako da ona generira događaj MojDogađaj
     // TODO: Implementirajte kod za Svojstvo
-    public class Implementacija
+    public class Implementacija : IMojeSučelje
+
     {
+        private string nešto;
+        public string Svojstvo
+        {
+            get; set;
+           
+           
+        }
+
+        public event MojDelegat MojDogađaj;
+
+        public void GenerirajMojDogađaj()
+        {
+            if (MojDogađaj != null)
+                MojDogađaj(this, EventArgs.Empty);
+                
+        }
     }
 
     public class DefinicijaImplementacijaSučelja
@@ -47,7 +64,7 @@ namespace Vsite.CSharp
         static void Main(string[] args)
         {
             // TODO: Inicijalizirajte objekt ms instancom objekta Implementacija i provjerite što će se ispisati izvođenjem koda
-            IMojeSučelje ms = null;
+            IMojeSučelje ms = new Implementacija();
 
             IspišiSvojstvo(ms);
             DigniDogađaj(ms);
