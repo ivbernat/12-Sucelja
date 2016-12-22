6ca0c0c6336fd028d97950ed2892ca8b47eef8df
Ivan Bernatović <ivbernat@vsite.hr>
2016-12-22 19:49:47 +0100

    implementacija sučelja

--- ImplementacijaSucelja/Osoba.cs
+++ ImplementacijaSucelja/Osoba.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Vsite.CSharp
 {
-    public class Osoba
+    public class Osoba : IComparable<Osoba>
     {
         public Osoba(string ime, string prezime, DateTime datumRoðenja, string mjestoRoðenja)
         {
@@ -15,6 +15,15 @@ namespace Vsite.CSharp
         public override string ToString()
         {
             return string.Format("{0} {1}, {2}, {3}", Ime, Prezime, DatumRoðenja.ToShortDateString(), MjestoRoðenja);
+        }
+
+        public int CompareTo(Osoba other)
+        {
+            int rezultat = Prezime.CompareTo(other.Prezime);
+            if (rezultat != 0)
+                return Ime.CompareTo(other.Ime);
+
+            throw new NotImplementedException();
         }
 
         public readonly string Ime;
